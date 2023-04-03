34ce7fe24ba9f57cdac4240c971889cb10eda0e6
Dan <sidato83@gmail.com>
2023-04-02 21:28:17 -0700

    Added TMPro to indicate selected player

--- Rat Race/Assets/Scripts/PlayerMovePositions.cs
+++ Rat Race/Assets/Scripts/PlayerMovePositions.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovePositions : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI playerIndicator;
     [SerializeField] private List<GameObject> players = new List<GameObject>();
     [SerializeField] private Positions movePositions = new Positions();
 
@@ -16,6 +18,8 @@ public class PlayerMovePositions : MonoBehaviour
     public void IsHuman()
     {
         isHuman = true;
+        playerIndicator.text = "HUMAN";
+
         isDog = false;
         isCat = false;
         isRat = false;
@@ -24,6 +28,8 @@ public class PlayerMovePositions : MonoBehaviour
     public void IsDog()
     {
         isDog = true;
+        playerIndicator.text = "DOG";
+
         isHuman = false;
         isCat = false;
         isRat = false;
@@ -32,6 +38,8 @@ public class PlayerMovePositions : MonoBehaviour
     public void IsCat()
     {
         isCat = true;
+        playerIndicator.text = "CAT";
+
         isHuman = false;
         isDog = false;
         isRat = false;
@@ -40,6 +48,8 @@ public class PlayerMovePositions : MonoBehaviour
     public void IsRat()
     {
         isRat = true;
+        playerIndicator.text = "RAT";
+
         isHuman = false;
         isDog = false;
         isCat = false;
