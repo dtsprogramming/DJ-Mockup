26c887214d75367470c557414f7058420a7088bc
Dan <sidato83@gmail.com>
2023-04-03 22:42:14 -0700

    Modified player movement and refactored move code
    Restricted each players movement to immediate neighboring tiles. Refactored the move code for readability.

--- Rat Race/Assets/Scripts/PlayerMovePositions.cs
+++ Rat Race/Assets/Scripts/PlayerMovePositions.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -7,88 +6,40 @@ public class PlayerMovePositions : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI playerIndicator;
     [SerializeField] private List<GameObject> players = new List<GameObject>();
-    [SerializeField] private Positions movePositions = new Positions();
 
-    private bool isHuman = false;
-    private bool isDog = false;
-    private bool isCat = false;
-    private bool isRat = false;
+    private int playerIndex;
 
     #region Player Selection
     public void IsHuman()
     {
-        isHuman = true;
+        playerIndex = 0;
         playerIndicator.text = "HUMAN";
-
-        isDog = false;
-        isCat = false;
-        isRat = false;
     }
 
     public void IsDog()
     {
-        isDog = true;
+        playerIndex = 1;
         playerIndicator.text = "DOG";
-
-        isHuman = false;
-        isCat = false;
-        isRat = false;
     }
 
     public void IsCat()
     {
-        isCat = true;
+        playerIndex = 2;
         playerIndicator.text = "CAT";
-
-        isHuman = false;
-        isDog = false;
-        isRat = false;
     }
 
     public void IsRat()
     {
-        isRat = true;
+        playerIndex = 3;
         playerIndicator.text = "RAT";
-
-        isHuman = false;
-        isDog = false;
-        isCat = false;
     }
     #endregion
 
     public void MoveSelectedPlayer(Transform movePos)
     {
-        if (isHuman)
+        if ((movePos.position - players[playerIndex].transform.position).magnitude < 3)
         {
-            players[0].transform.position = movePos.transform.position;
-        }
-
-        if (isDog)
-        {
-            players[1].transform.position = movePos.transform.position;
-        }
-
-        if (isCat)
-        {
-            players[2].transform.position = movePos.transform.position;
-        }
-
-        if (isRat)
-        {
-            players[3].transform.position = movePos.transform.position;
-        }
-    }
-
-    [System.Serializable]
-    public class Positions
-    {
-        [SerializeField] private List<MovePosNeighbors> positionList = new List<MovePosNeighbors>();
-    }
-
-    [System.Serializable]
-    public class MovePosNeighbors
-    {
-        [SerializeField] private List<GameObject> position = new List<GameObject>();
-        [SerializeField] private List<GameObject> neighbors = new List<GameObject>();
+            players[playerIndex].transform.position = movePos.position;
+        };
     }
 }
